9e4cbe9012fac1b4311a3bb0936511236c388abf
André Machado <csantos.machado@gmail.com>
2020-10-20 20:18:36 +0000

    Update models

--- backend/newapptst19oct_13570/wsgi.py
+++ backend/newapptst19oct_13570/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newapptst19oct_13570.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newapptst19oct_13570.settings")
 
 application = get_wsgi_application()
